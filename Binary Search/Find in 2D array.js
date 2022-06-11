b0542f8dc46851eca1b7fa238281109b9fcad271
Animesh Dey <animesh.dey986@gmail.com>
2022-06-12 02:21:37 +0530

    roatate count

---
+++ Binary Search/Find in 2D array.js
@@ -0,0 +1,22 @@
+//Find the target element in sorted multidimentional array.
+{
+    function findElement(arr, target) {
+        let row = 0, col = arr.length - 1
+        while (row < arr.length && col >= 0) {
+            if (arr[row][col] == target) {
+                return `${row},${col}`                              //[row,col]
+            }
+            if (arr[row][col] < target) {
+                row++
+            } else {
+                col--
+            }
+        }
+        return `-1,-1`
+    }
+    console.log(findElement([
+        [10, 12, 16, 19],
+        [13, 18, 25, 28],
+        [16, 20, 27, 35],
+        [20, 24, 30, 38]], 26))
+}
